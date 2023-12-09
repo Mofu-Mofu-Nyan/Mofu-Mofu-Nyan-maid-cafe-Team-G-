3c02a068e0b0feaf095f17412477d34718120e2d
racruzliga <150246855+racruzliga@users.noreply.github.com>
2023-12-09 17:36:40 +0000

    Add files via upload

---
+++ Mafu mafu/JavaScript/script.js
@@ -0,0 +1,25 @@
+
+/*java script for slides in location*/	
+var slideIndex = 0;
+			showSlides();
+
+			function showSlides() {
+			  var i;
+			  var slides = document.getElementsByClassName("mySlides");
+			  for (i = 0; i < slides.length; i++) {
+				slides[i].style.display = "none";
+			  }
+			  slideIndex++;
+			  if (slideIndex > slides.length) {
+				slideIndex = 1;
+			  }
+			  slides[slideIndex - 1].style.display = "block";
+			  setTimeout(showSlides, 2000); // Change slide every 2 seconds
+			}
+			
+
+	
+function myAlert() {
+alert ("Thanks for accepting the rule book. Enjoy!!!");
+}
+
