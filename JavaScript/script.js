e038cd3646d1a31beecd383b965e4d074aa621e2
Esmeralda Humerez <cris4hz@gmail.com>
2023-12-10 01:59:13 +0000

    final

--- JavaScript/script.js
+++
@@ -1,9 +0,0 @@
-const i_menu = document.getElementById("i_menu");
-const nav_bar = document.querySelector(".nav_bar");
-const homepage = document.querySelector(".homepage");
-
-i_menu.addEventListener("click",()=>{
-    nav_bar.classList.toggle("close");
-    homepage.classList.toggle("close");
-});
-
